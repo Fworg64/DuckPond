bd6ae42dcd9fdf2c043743ea962030991bc5433f
Fworg64 <fworg64@gmail.com>
2016-02-07 02:29:31 +0100

    got the duck to move!
    Added flick to duck and a basic input listener to gamescreen

--- core/src/com/fworg64/duckpond/game/Duck.java
+++ core/src/com/fworg64/duckpond/game/Duck.java
@@ -15,23 +15,34 @@ public class Duck
 
     Vector2 vel;
     Vector2 accl;
+    Vector2 posv;
 
     public Duck()
     {
         pos = new Rectangle(-100,50,96,96); //make this random for default constructor
 
-        vel = new Vector2(.5f, .2f); //must be floats...
+        vel = new Vector2(50.0f, 20.0f); //must be floats... measured in whatever/sec
         accl = new Vector2(0,0);
+        posv = new Vector2(-100,50);
 
     }
 
     public void update(float delta)
     {
-        vel.add(accl);
-        pos.setPosition(new Vector2(pos.getX(), pos.getY()).add(vel));
+        if (accl.len2() >= 1) accl.scl(.95f); //damping of accl, if its bigger than a small number, shrink it. taking the len2 is faster than the len
+        else accl.setZero(); // if its smaller, zero it.
+
+        vel.add(accl.cpy().scl(delta)); //velocity + accel * time = new vel
+        posv.add(vel.cpy().scl(delta)); //nother vector for good measure
+        pos.setPosition(posv); //pos + vel*time = new pos
 
         //stuff to determine frame of animation
 
+    }
+
+    public void flick (Vector2 flickv)
+    {
+        accl.set(flickv.clamp(500,2000)); //clamp flick to min/max floats
     }
 
 }
