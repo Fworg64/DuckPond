67915842a36c7b1f165000992115fcfb0c1b948d
Fworg64 <fworg64@gmail.com>
2016-02-09 17:00:20 +0100

    Moved Assets and Loading into an Assets File
    All textures should be referenced from and added to this assets class

--- core/src/com/fworg64/duckpond/game/DuckPondGame.java
+++ core/src/com/fworg64/duckpond/game/DuckPondGame.java
@@ -11,18 +11,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DuckPondGame extends Game {
 	public SpriteBatch batch;
-	public static BitmapFont font;
 
 	public String debug; //probably make a debug file too...
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		font = new BitmapFont();
-		font.setColor(Color.FIREBRICK); //eventually get a real font and put in assets
+		Assets.load();
 
 		debug = "herpp";
-
 
 		setScreen(new MainMenuScreen(this));
 	}
@@ -30,10 +27,5 @@ public class DuckPondGame extends Game {
 	@Override
 	public void render () {
 		super.render();
-		/*Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();*/
 	}
 }
