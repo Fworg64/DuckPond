12dbd7587dd5b58d0b00c3d38d42155c9e145da9
Fworg64 <fworg64@gmail.com>
2016-02-10 01:45:32 +0100

    Added the lillypad
    Still need collision detection

---
+++ core/src/com/fworg64/duckpond/game/Lily.java
@@ -0,0 +1,21 @@
+package com.fworg64.duckpond.game;
+
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * Created by fworg on 2/10/2016.
+ */
+public class Lily
+{
+    Rectangle pos;
+
+    public Lily()
+    {
+        pos = new Rectangle(160, 360, 96, 96);
+    }
+
+    public void update()
+    {
+        //stuff to determine frame of animation
+    }
+}
