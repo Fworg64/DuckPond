d48663f06c344441e649e69a3aea4f1009636eb6
Fworg64 <fworg64@gmail.com>
2016-02-20 02:18:32 +0100

    disabled level edit screen for time being
    didn't compile

--- core/src/com/fworg64/duckpond/game/MainMenuScreen.java
+++ core/src/com/fworg64/duckpond/game/MainMenuScreen.java
@@ -101,8 +101,8 @@ public class MainMenuScreen extends ScreenAdapter
                 }
                 if (leveleditbutt.contains(touchpoint.x, touchpoint.y))
                 {
-                    game.setScreen(new LevelScreen(game));
-                    return 1;
+                    //game.setScreen(new LevelScreen(game));
+                    //return 1;
                 }
                 if (optionbutt.contains(touchpoint.x, touchpoint.y))
                 {
