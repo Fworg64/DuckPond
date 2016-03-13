3f4e77d4308b458ba074e600fb02b50baf893d55
Fworg64 <fworg64@gmail.com>
2016-03-13 16:22:13 +0100

    changed version # and todo
    :cactus: :cake: :yum:

--- core/src/com/fworg64/duckpond/game/DuckPondGame.java
+++ core/src/com/fworg64/duckpond/game/DuckPondGame.java
@@ -24,7 +24,7 @@ public class DuckPondGame extends Game {
 	public static int spriteW = 48;
 	public static int spriteH = 48;
 
-	public static final String version = "v0.0.5a";
+	public static final String version = "v0.0.7a";
 
 	public SpriteBatch batch;
     public MusicAndSounds mas;
